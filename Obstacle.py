a387cfe5b6b3f0cf46cf8f20b0a9e85a47d87a99
hujunhan <3160102202@zju.edu.cn>
2019-03-21 13:26:19 +0800

    Change Robot.py

--- Obstacle.py
+++ Obstacle.py
@@ -3,10 +3,11 @@ import socket
 import random
 import sys
 import numpy as np
-from SSL_Lib import vision_detection_pb2
 from SSL_Lib.Robot import *
 from SSL_Lib.Camera import *
 from SSL_Lib.utils import *
+from SSL_Lib import vision_detection_pb2
+
 # 初始化控制和读取的IP地址、端口号
 control_addr = ('127.0.0.1', 20011)
 read_addr = ('127.0.0.1', 23333)
@@ -17,7 +18,7 @@ def chase(robot, goal):
         if ro.id in blue:
             if goal.id in blue:
                 angle = calc_angle(blue[ro.id], blue[goal.id])
-                ro.setSpeed(0, 1, -angle+np.random.uniform(-0.1,0.1))
+                ro.setSpeed(0, 1, -angle)
 
 
 def random_robot(robot):
@@ -34,3 +35,4 @@ camera = Camera(read_addr)
 while True:
     blue, yellow = camera.getRobotDict()
     chase(obstacle, goal)
+    time.sleep(0.01)
