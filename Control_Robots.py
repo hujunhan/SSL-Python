8fade8cf211b9046c0cd5320be0cc419d0bd6c64
xubowen123 <3160102492@zju.edu.cn>
2019-03-28 08:51:20 +0800

    缓慢爬行

--- Control_Robots.py
+++ Control_Robots.py
@@ -2,33 +2,55 @@ import time
 import socket
 import sys
 from SSL_Lib import vision_detection_pb2
-import numpy as np
 from SSL_Lib.Robot import *
 from SSL_Lib.Camera import *
 from SSL_Lib.P2P import *
-import matplotlib.pyplot as plt
-from SSL_Lib.DStar import DStar
-import numpy as np
+from SSL_Lib.DStar import *
 # 初始化控制和读取的IP地址、端口号
-control_addr = ('127.0.0.2', 20011)
+control_addr = ('127.0.0.1', 20011)
 read_addr = ('127.0.0.1', 23334)
 
 ro_b_0 = Robot("blue", 0,0.15,control_addr)
+ro_b_0.sim = True
+
 camera=Camera(read_addr)
-
 x,y,ori=camera.getRobotPos()
-goal=[10,10]
-pf = DStar(x_start=int(-400), y_start=int(0), x_goal=goal[0], y_goal=goal[1])
-for i in range(1,13):
-    pf.set_obstract(round(x[i]*100),round(y[i]*100),15)
-pf.replan()
-##至此 静态路径规划完成
-print(len(pf.path))
-print(pf.path)
-while True:
 
-    success=0
-#time_start = time.time()  # 记录测试开始的时间
-for index in range(len(pf.path)-1):
-    xx,yy,success = P2P(ro_b_0,camera,pf.path[index].x/100,pf.path[index].y/100)
-    print("index=",index,"success=",success,"nowx=",x[0],"nowy=",y[0],"wantx=",pf.path[index].x/100,"wanty=",pf.path[index].y/100)
+#ro_b_0.setSpeed(0, 1, 0)  # 设置要测试的命令
+time_start = time.time()  # 记录测试开始的时间
+x, y, ori = camera.getRobotPos()
+dest_x=0
+dest_y=0
+vy=0
+vx=0
+distance0=((x[0]-dest_x)**2+(y[0]-dest_y)**2)**0.5
+ay=(dest_x-x[0])/distance0/20
+ax=(dest_y-y[0])/distance0/20
+print(x[0])
+pf = DStar((int)(x[0]*100), (int)(y[0]*100), 0, 0)
+pf.set_obstract(-250,0,100)  
+pf.set_obstract(-150,0,100)  
+pf.set_obstract(-50,0,100)  
+pf.replan() 
+i=0
+print(pf.get_path())
+print(pf.get_path()[i].y)
+len=pf.get_path().__len__()
+path_x=x[0]
+path_y=y[0]
+while True:
+    if i is len or i > len:
+        break
+    if (x[0]-pf.get_path()[i].x/100)**2+(y[0]-pf.get_path()[i].y/100)**2<0.002:
+        i=i+1
+        pass
+    else:
+        path_x=pf.get_path()[i].x/100
+        path_y=pf.get_path()[i].y/100 
+    
+    xx,yy,success = P2P(ro_b_0,camera,path_x,path_y)
+    x,y,ori=camera.getRobotPos()
+    print("success=",success,"x=",x[0],"y=",y[0])
+    if success is 1:
+        i=i+1
+
--- SSL_Lib/P2P.py
+++ SSL_Lib/P2P.py
@@ -11,21 +11,21 @@ def P2P(robot,camera,dest_x,dest_y):
         x, y, ori = camera.getRobotPos()
         distance=((x[0]-dest_x)**2+(y[0]-dest_y)**2)**0.5
         if distance>distance0+0.5:
-            setSpeed(0,0,0)
+            robot.setSpeed(0,0,0)
             return x,y,-1
         if(distance>0.5*distance0):
             if abs(vx)<2 and abs(vy)<2:
                 vx=vx+ax
                 vy=vy+ay
-        else:
-            if abs(vx)>ax and abs(vy)>ay:
-                vx=vx-ax
-                vy=vy-ay
-            else:
-                vx,vy=0,0
-                robot.setSpeed(vx,vy,w)
-                x, y, ori = camera.getRobotPos()
-                return x,y,0#0表示未到达目的地，1表示成功到达
+       # else:
+       #     if abs(vx)>ax and abs(vy)>ay:
+       #         vx=vx-ax
+       #         vy=vy-ay
+       #     else:
+       #         vx,vy=0,0
+       #         robot.setSpeed(vx,vy,w)
+       #         x, y, ori = camera.getRobotPos()
+       #         return x,y,0#0表示未到达目的地，1表示成功到达
         robot.setSpeed(vx,vy,w)
         #print("vx=",vx,"vy=",vy,"w=",w)
         #print("x=",x[0],"y=",y[0],"distance=",distance)
