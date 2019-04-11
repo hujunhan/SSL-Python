647752d71756313742b5b93d1623724b08206017
Junhan Hu <h793627866@gmail.com>
2019-04-11 13:24:53 +0800

    add script for yellow car

--- rrt-dynamic-yellow.py
+++ rrt-dynamic-yellow.py
@@ -25,10 +25,10 @@ debug = DBG()
 
 
 # 1.初始化要控制的机器人
-ro_b_0 = Robot('blue', 0, 0.15, control_addr=control_addr)
+ro_b_0 = Robot('yellow', 0, 0.15, control_addr=control_addr)
 
 blue, yellow = camera.getRobotDict()  # 读取初始信息
-start_point = [blue[0].x, blue[0].y]  # 设置机器人开始的位置
+start_point = [yellow[0].x, yellow[0].y]  # 设置机器人开始的位置
 end_point = [-start_point[0], -start_point[1]]  # 设置机器人终点为对称点
 print('start at: ', start_point)
 print('goal at: ', end_point)
@@ -36,11 +36,11 @@ print('goal at: ', end_point)
 # 2.目前只测试静态避障，所以只生成一次路径规划
 ob_temp = []
 for ro in blue.values():
-	if ro.robot_id is not 0:
-		ob_temp.append((ro.x, ro.y, 0.3))
+	ob_temp.append((ro.x, ro.y, 0.3))
 # debug.addCircle(ro.x/10,ro.y/10,20)
 for ro in yellow.values():
-	ob_temp.append((ro.x, ro.y, 0.3))
+	if ro.robot_id is not 0:
+		ob_temp.append((ro.x, ro.y, 0.3))
 # debug.addCircle(ro.x/10,ro.y/10,20)
 ob = np.array(ob_temp)
 print('ob = ', ob_temp)
@@ -48,13 +48,13 @@ u = np.array([0.0, 0.0])
 config = Config()
 # 2.1 新建地图
 starttime = time.time()
-rrt = RRT(start=[blue[0].x, blue[0].y], goal=end_point,
+rrt = RRT(start=[yellow[0].x, yellow[0].y], goal=end_point,
 		  randArea=[6, 4], obstacleList=ob)
 path = rrt.Planning(animation=False)
 maxIter = 4000
 path = PathSmoothing(path, maxIter, ob_temp)
 print('Using ', time.time() - starttime, ' to calculate rrt!')
-x = np.array([blue[0].x, blue[0].y, blue[0].orientation, 0.0, 0.0])
+x = np.array([yellow[0].x, yellow[0].y, yellow[0].orientation, 0.0, 0.0])
 traj = np.array(x)
 # while path is None:  # 如果障碍物膨胀太多，就逐渐减小
 # 	radius = radius - 0.01
@@ -92,18 +92,18 @@ def getblue0():
 		blue, yellow = camera.getRobotDict()
 		ob_temp1 = []
 		for ro in blue.values():
-			if ro.robot_id is not 0:
-				ob_temp1.append([ro.x, ro.y, 0.3])
+			ob_temp1.append([ro.x, ro.y, 0.3])
 		# debug.addCircle(ro.x/10,ro.y/10,20)
 		for ro in yellow.values():
-			ob_temp1.append(([ro.x, ro.y, 0.3]))
+			if ro.robot_id is not 0:
+				ob_temp1.append(([ro.x, ro.y, 0.3]))
 		# debug.addCircle(ro.x/10,ro.y/10,20)
 		ob = np.array(ob_temp1)
 
 
 def checkDanger():
-	sx = blue[0].x
-	sy = blue[0].y
+	sx = yellow[0].x
+	sy = yellow[0].y
 	gx = goal[0]
 	gy = goal[1]
 	for ro in ob:
@@ -142,7 +142,7 @@ while True:
 		else:
 			xx = start_point
 		GOAL_FLAG = ~GOAL_FLAG
-		rrt = RRT(start=[blue[0].x, blue[0].y], goal=xx,
+		rrt = RRT(start=[yellow[0].x, yellow[0].y], goal=xx,
 				  randArea=[6, 4], obstacleList=ob)
 		path = rrt.Planning(animation=False)
 		maxIter = 4000
@@ -152,15 +152,15 @@ while True:
 		k = -1
 	# 4.1 根据DWA计算所应该施加的控制指令
 	# u[0]是机器人x轴速度，u[1]是机器人y轴速度
-	angle = togoal(blue[0], goal)
+	angle = togoal(yellow[0], goal)
 	ro_b_0.setSpeed(speed * np.sin(angle), speed * np.cos(angle), 0)
 	# u, ltraj = dwa_control(x, u, config, goal, ob, ro_b_0, camera)
 	# print(u)
 	# if np.hypot(traj[-1,0]-traj[0,0],traj[-1,1]-traj[0,1]) <0.1:
-	# 	pf = statics_map([blue[0].x, blue[0].y], end_point, blue, yellow, radius)
+	# 	pf = statics_map([yellow[0].x, yellow[0].y], end_point, blue, yellow, radius)
 	# 	pf.shorter_the_path(2, 10)
 	# 	path = pf.get_path()
-	distance = math.sqrt((blue[0].x - goal[0]) ** 2 + (blue[0].y - goal[1]) ** 2)
+	distance = math.sqrt((yellow[0].x - goal[0]) ** 2 + (yellow[0].y - goal[1]) ** 2)
 	if distance <= 1:
 		speed = 0.2 + 2.3 * distance
 		if distance <= 0.1:
@@ -169,7 +169,7 @@ while True:
 					xx = end_point
 				else:
 					xx = start_point
-				rrt = RRT(start=[blue[0].x, blue[0].y], goal=xx,
+				rrt = RRT(start=[yellow[0].x, yellow[0].y], goal=xx,
 						  randArea=[6, 4], obstacleList=ob)
 				path = rrt.Planning(animation=False)
 				maxIter = 1000
@@ -188,4 +188,4 @@ while True:
 	# debug.addpath_dwa(ltraj)
 	debug.sendDebugMessage()  # debug信息发送
 # time.sleep(0.015)
-# chase2(blue[0],[path_x,path_y],ro_b_0,1,1)
+# chase2(yellow[0],[path_x,path_y],ro_b_0,1,1)
